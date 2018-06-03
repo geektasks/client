d8ed2c21cde85ecc2e3726baeb3d886c79701109
Pegorino82 <eshkryabin@yandex.ru>
2018-06-03 15:30:22 +0300

    local commit

--- function/check.py
+++ function/check.py
@@ -1,15 +1,14 @@
 from client.client import send_queue
+from jim.jimrequest import JIMRequest
+import hashlib
 
 queue = send_queue
+secret = 'secret_key'
+
+session = JIMRequest()
+
 
 def check_user(name):
-    message = {
-        "head": {
-            "type": "action",
-            "name": "check_user"
-        },
-        "body": {
-            "name": name
-        }
-    }
+
+    message = session.check_user(name=name).jim_dict
     queue.put(message)
