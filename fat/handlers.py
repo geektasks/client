cbddff81429de3abbe76650552ef085c1fb68640
Pegorino82 <eshkryabin@yandex.ru>
2018-06-11 23:40:27 +0300

    сделал обработчики сообщений регистрации/авторизации/создания задачи/редактирования задачи
    регистрация/авторизация/создание задачи работают, редактирование не проверил

--- fat/handlers.py
+++ fat/handlers.py
@@ -1,4 +1,6 @@
 import fat.handlers_base as handlers_base
+from db.client_db import ClientDB
+from task.task import Task
 
 """
 Инструкция по созданию функций-обработчиков сообщений
@@ -83,11 +85,20 @@ D. Функция, вызываемая в случае отключенного
 * декорируемые функции могут иметь одинаковые названия
 """
 
-# handler = handlers_base.FatThing("127.0.0.1", 8888, "my_db.sqlite")
-# handler = handlers_base.FatThing("127.0.0.1", 8000, "my_db.sqlite")
+handler = handlers_base.FatThing("127.0.0.1", 8000)
 
 
-handler = handlers_base.FatThing("ddimans.dyndns.org", 8000, "my_db.sqlite")
+# handler = handlers_base.FatThing("ddimans.dyndns.org", 8000)
+
+
+# handler = handlers_base.FatThing("127.0.0.1", 8888)
+
+
+@handler.init_func
+def init_func():
+    # Инициализируем БД
+    data["db"] = ClientDB.create_db("my_db.sqlite")
+    data["db"].connect()
 
 
 @handler.conditional_queue_handler("action", "registration")
@@ -96,6 +107,17 @@ def registration(message):
     data['username'] = username
     block_queue()
     send_message(message)
+
+
+@handler.conditional_socket_handler('server response', 'registration')
+def registration(message):
+    if message['body']['code'] == 201:
+        data['db'].add_user(data['username'])
+    else:
+        data['username'] = None
+        print(message['body']['code'], message['body']['message'])
+    put_message(message)
+    release_queue()
 
 
 @handler.conditional_queue_handler("action", "authorization")
@@ -108,25 +130,71 @@ def authorization(message):
 
 @handler.conditional_socket_handler("server response", "authorization")
 def authorization(message):
-    session_id = message['body']['session id']
-    data['session_id'] = int(session_id)
-    db.add_session_id(data['username'], session_id)
+    if message['body']['code'] == 200:
+        session_id = message['body']['session id']
+        data['session_id'] = int(session_id)
+        data['db'].add_session_id(data['username'], session_id)
+    else:
+        data['username'] = None
+        print(message['body']['code'], message['body']['message'])
     put_message(message)
     release_queue()
 
 
 @handler.conditional_queue_handler('action', 'create task')
 def create_task(message):
-    print('cr task', message)
+    print('create task ->', message)
+    data['create_task'] = message
     block_queue()
     send_message(message)
+
+
+@handler.conditional_socket_handler('server response', 'create task')
+def create_task(message):
+    if message['body']['code'] == 201:
+        creator = data['username']
+        # task_id = data['create_task']['body'].get('id')
+        task_name = data['create_task']['body'].get('name')
+        task_description = data['create_task']['body'].get('description')
+        data.pop('create_task')
+
+        task = Task(creator=creator, viewer=creator, name=task_name)
+        task.description = task_description
+        task = task.task_dict
+        print(task)
+
+        data['db'].add_task(task)
+    else:
+        print(message['body']['code'], message['body']['message'])
+
+    put_message(message)
+    release_queue()
 
 
 @handler.conditional_queue_handler('action', 'edit task')
-def create_task(message):
-    print('edit task', message)
+def edit_task(message):
+    print('edit task ->', message)
+    data['edit_task'] = message
     block_queue()
+    print(1)
     send_message(message)
+    print(2)
 
-# @handler.conditional_socket_handler('server response', 'check user')
-# def check_user(message):
+
+@handler.conditional_socket_handler("server response", "edit task")
+def edit_task(message):
+    if message['body']['code'] == 200:
+        task_name = data['edit_task']['body'].get('name')
+        task_description = data['edit_task']['body'].get('description')
+        task_id = data['db'].get_task_id_by_name(task_name=task_name)
+        data.pop('edit_task')
+        if task_name:
+            data['db'].change_task_name(task_id=task_id, task_name=task_name)
+        if task_description:
+            data['db'].change_task_description(task_id=task_id, task_description=task_name)
+
+    else:
+        print(message['body']['code'], message['body']['message'])
+
+    put_message(message)
+    release_queue()
