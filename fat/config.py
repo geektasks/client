ad8af3b090261a4a82b8ac02159246703d72f75a
Pegorino82 <eshkryabin@yandex.ru>
2018-06-28 20:31:44 +0300

    ctrl alt L

--- fat/config.py
+++ fat/config.py
@@ -3,6 +3,6 @@ import fat.handlers_base as handlers_base
 
 def connect_address():
     # return  handlers_base.FatThing("10.72.72.30", 8000)
-    return  handlers_base.FatThing("127.0.0.1", 8000)
+    return handlers_base.FatThing("127.0.0.1", 8000)
     # handler = handlers_base.FatThing("ddimans.dyndns.org", 8000)
     # handler = handlers_base.FatThing("185.189.12.43", 8000)
